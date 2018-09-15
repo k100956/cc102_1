2ef452acfed66486ea812963631e067a530001d1
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-15 17:12:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
  set -ex
-      USERName=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
-      IMAGE=testUser
-      version=`cat VERSION`
-      echo "version: $version"
-      docker build -t $userName/$image:latest .
-      docker tag $userName/$image:latest $userName/$image:$version
+ USERName=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
+ IMAGE=testUser
+ version=`cat VERSION`
+ echo "version: $version"
+ docker build -t $userName/$image:latest .
+ docker tag $userName/$image:latest $userName/$image:$version
