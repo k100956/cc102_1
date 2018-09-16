071e3cf9d9a8a304b7c8e9a3f7a76a4d5c5cb8c6
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-17 00:05:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ set -ex
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_ecr_test
-docker build -t api -f ./dockerfile/dockerfile-api
-docker build -t mysql -f ./dockerfile/dockerfile-mysql
-docker build -t line -f ./dockerfile/dockerfile-line
-docker build -t dns -f ./dockerfile/dockerfile-dns
-docker build -t iii -f ./dockerfile/dockerfile-iii
+docker build -t api  ./dockerfile-api/
+docker build -t mysql  ./dockerfile/
+docker build -t line  ./dockerfile/
+docker build -t dns  ./dockerfile/
+docker build -t iii  ./dockerfile/
 #docker-compse up -d
 docker tag api $USERNAME/$IMAGE:api
 docker tag mysql $USERNAME/$IMAGE:mysql
