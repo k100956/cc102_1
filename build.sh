8fe4460a400857e9022ba9fa15f248216c998c28
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-16 23:12:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,23 @@ set -ex
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_ecr_test
-docker build -t $USERNAME/$IMAGE:latest .
-version=`cat VERSION`
-echo "version: $version"
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-docker push $USERNAME/$IMAGE:$version
+docker build -t api -f ./dockerfile/dockerfile-api
+docker build -t mysql -f ./dockerfile/dockerfile-mysql
+docker build -t line -f ./dockerfile/dockerfile-line
+docker build -t dns -f ./dockerfile/dockerfile-dns
+docker build -t iii -f ./dockerfile/dockerfile-iii
+docker tag api $USERNAME/$IMAGE:api
+docker tag mysql $USERNAME/$IMAGE:mysql
+docker tag line $USERNAME/$IMAGE:line
+docker tag dns $USERNAME/$IMAGE:dns
+docker tag iii $USERNAME/$IMAGE:iii
+
+
+
+
+#docker build -t $USERNAME/$IMAGE:latest .
+#version=`cat VERSION`
+#echo "version: $version"
+#docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+
 
