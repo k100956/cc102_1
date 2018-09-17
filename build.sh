d012710facba02635a0d46a4ac07dfe940e0027c
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-17 18:11:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,21 +3,15 @@ set -ex
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_ecr_test
-docker build -t api  ./dockerfile-api/
-docker build -t mysql  ./dockerfile/
-docker build -t line  ./dockerfile/
-docker build -t dns  ./dockerfile/
-docker build -t iii  ./dockerfile/
+docker build -t $USERNAME/$IMAGE:mysql -f Chatbot_Dev/dockerfile-mysql Chatbot_Dev/
+docker build -t $USERNAME/$IMAGE:api -f Chatbot_Dev/dockerfile-api Chatbot_Dev/
+docker build -t $USERNAME/$IMAGE:redis -f Chatbot_Line/dockerfile-redis Chatbot_Line/
+docker build -t $USERNAME/$IMAGE:jupyter -f Chatbot_Line/dockerfile-jupyter Chatbot_Line/
+docker build -t $USERNAME/$IMAGE:ngrok -f Chatbot_Line/dockerfile-ngrok Chatbot_Line/
+
+
 #docker-compse up -d
-docker tag api $USERNAME/$IMAGE:api
-docker tag mysql $USERNAME/$IMAGE:mysql
-docker tag line $USERNAME/$IMAGE:line
-docker tag dns $USERNAME/$IMAGE:dns
-docker tag iii $USERNAME/$IMAGE:iii
-
-
-
-
+#docker tag iii $USERNAME/$IMAGE:iii
 #docker build -t $USERNAME/$IMAGE:latest .
 #version=`cat VERSION`
 #echo "version: $version"
